746075309b74de02d7759d98bf189d928dcf5043
Anthony <lightweightdevelopment@gmail.com>
2015-12-02 18:54:03 +0000

    Working Kube

--- practical/kubernetes_demo/install.sh
+++ practical/kubernetes_demo/install.sh
@@ -9,7 +9,7 @@
 
 #===================================================================================
 
-wget https://storage.googleapis.com/kubernetes-release/release/v0.19.3/bin/linux/amd64/kubectl && \
+wget https://storage.googleapis.com/kubernetes-release/release/v1.0.1/bin/linux/amd64/kubectl && \
   chmod +x kubectl && \
   sudo mv kubectl /usr/local/bin && \
   kubectl create -f ./kube/redis-master-service.yaml && \
