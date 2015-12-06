769783763814269629dbedcaed3c276343db4d43
Anthony <lightweightdevelopment@gmail.com>
2015-12-06 15:05:43 +0000

    Added marcelos PR

--- practical/swarm_demo/install.sh
+++ practical/swarm_demo/install.sh
@@ -5,19 +5,28 @@
 #
 # USAGE: chmod +x ./install.sh && ./install.sh
 #
-# DESCRIPTION: Tears down an existing Swarm hosts and
-#              provisions a fresh cluster.
-
+# DESCRIPTION: Creates a Docker Swarm cluster with a discovery service node (Consul),
+#              a master and two slave nodes and runs the CA674 demo app on top of it.
+#              The CA674 demo is comprised of a back-end store (redis) and front-end
+#              www implementation (python) scaled to initially two instances.
+#              A load balancer is used to randomize access to each front-end container
+#              and unify the entry point of the app.
+#
 #===================================================================================
 
-VM=local
+function teardown_swarm_machines {
+  echo "Removing old Swarm machines..."
+  docker-machine stop keystore
+  docker-machine stop swarm-master
+  docker-machine stop swarm-node-01
+  docker-machine stop swarm-node-02
+  docker-machine rm -f keystore
+  docker-machine rm -f swarm-master
+  docker-machine rm -f swarm-node-01
+  docker-machine rm -f swarm-node-02
+}
 
 function provision_machines {
-    echo "Entering $VM machine..."
-    docker-machine status $VM || docker-machine create -d virtualbox $VM && sleep 2
-    docker-machine start $VM && sleep 2
-    eval "$(docker-machine env $VM)"
-
     echo "Creating key-value store machine..."
     docker-machine create -d virtualbox keystore
 
@@ -25,13 +34,16 @@ function provision_machines {
     docker $(docker-machine config keystore) run -d -p "8500:8500" -h "consul" progrium/consul -server -bootstrap
 
     echo "Creating Swarm master..."
-    docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-advertise=eth1:2376" swarm-master
+    docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery="consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-advertise=eth1:2376" swarm-master && sleep 2
+    docker-machine start swarm-master && sleep 20
 
     echo "Creating Swarm node 01..."
-    docker-machine create -d virtualbox --swarm --swarm-discovery="consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-advertise=eth1:2376" swarm-node-01
+    docker-machine create -d virtualbox --swarm --swarm-discovery="consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-advertise=eth1:2376" swarm-node-01 && sleep 2
+    docker-machine start swarm-node-01 && sleep 2
 
     echo "Creating Swarm node 02..."
-    docker-machine create -d virtualbox --swarm --swarm-discovery="consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-advertise=eth1:2376" swarm-node-02
+    docker-machine create -d virtualbox --swarm --swarm-discovery="consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-store=consul://$(docker-machine ip keystore):8500" --engine-opt="cluster-advertise=eth1:2376" swarm-node-02 && sleep 2
+    docker-machine start swarm-node-02 && sleep 2
 
     echo "Creating overlay network..."
     eval "$(docker-machine env --swarm swarm-master)"
@@ -61,14 +73,17 @@ function start_services {
     echo "System is operational. Access the URL: "$URL
 }
 
-function teardown_swarm_machines {
-    echo "Removing old Swarm machines..."
-    docker-machine status keystore && docker-machine rm -f keystore
-    docker-machine status swarm-master && docker-machine rm -f swarm-master
-    docker-machine status swarm-node-01 && docker-machine rm -f swarm-node-01
-    docker-machine status swarm-node-02 && docker-machine rm -f swarm-node-02
+function finishtobash {
+    echo "Finishing up. Setting environment variables for machine swarm-master..."
+    eval "$($DOCKER_MACHINE env --swarm swarm-master)"
+
+    clear
+    echo "CA674 - Docker Swarm demo app running on "$URL
+    echo "use 'docker ps' to see the running containers"
+    exec "$BASH" --login -i
 }
 
 teardown_swarm_machines
 provision_machines
 start_services
+finishtobash
