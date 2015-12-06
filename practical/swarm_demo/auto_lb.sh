5e2de2d32703d50f5610e7920e73d18e35ee2591
magrossi <magrossi@gmail.com>
2015-12-06 00:28:54 +0000

    New scripts
    Install.sh nows provisions machines with Consul and outputs the URL of
    the load balancer for direct testing. Two instances of the front-end are
    created.
    auto_lb.sh re-creates the load balancer with however many front-end
    instances that are running.

---
+++ practical/swarm_demo/auto_lb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#===================================================================================
+#
+# FILE: auto_lb.sh
+#
+# USAGE: ./auto_lb.sh
+#
+# DESCRIPTION: Auto configure and recreate the load balancer
+#
+#===================================================================================
+
+VM=swarm-master
+
+function connectswarm {
+	echo "Connecting to swarm environment..."
+    eval "$(docker-machine env --swarm $VM)"
+}
+
+function teardown {
+	echo "Tearing down old load balancer..."
+	docker-compose -f lb.yml stop && docker-compose -f lb.yml rm --force
+}
+
+function auto_loadbalance {
+    echo "Find front-end IP addresses and PORT.."
+    LB=`docker ps | grep -E -o "(([0-9]{1,3}[\.]){3}[0-9]{1,3})[\:][0-9]{1,5}(->5000/tcp)" | sed 's/:/ /' | sed 's/->5000\/tcp//' | xargs`
+    echo "Found: "$LB
+
+    echo "Updating lb.yml file with front-end addresses..."
+    sed -i -e 's/command:.*$/command: 80 '"$LB"'/' lb.yml
+
+    echo "Running load balancer..."
+    docker-compose -f lb.yml up -d
+
+    URL=`docker ps | grep -E -o "(([0-9]{1,3}[\.]){3}[0-9]{1,3})(:80->80/tcp)" | sed 's/:80->80\/tcp//'`
+    echo "System is operational. Access the URL: "$URL
+}
+
+connectswarm
+teardown
+auto_loadbalance
+read a
